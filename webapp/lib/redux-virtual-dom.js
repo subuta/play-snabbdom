576adc71d90b09c50a233a0c9d1070e60634c567
shunta saito <s.shunta@goodpatch.com>
2016-07-25 00:09:39 +0900

    [modify]fix syntax.

--- webapp/lib/redux-virtual-dom.js
+++ webapp/lib/redux-virtual-dom.js
@@ -6,35 +6,33 @@ export default function(store) {
   let cache = {};
 
   // injects store context to render function.
-  const inject = function(render, mapStateToProps, mapDispatchToProps) {
-    return (props = {}) => {
-      const state = getState();
+  const inject = (render, mapStateToProps, mapDispatchToProps) => (props = {}) => {
+    const state = getState();
 
-      // check mapper functions.
-      const isChanged = !cache[render] || !equal(cache[render].lastProps, props) || !equal(cache[render].lastState, state);
+    // check mapper functions.
+    const isChanged = !cache[render] || !equal(cache[render].lastProps, props) || !equal(cache[render].lastState, state);
 
-      // getResult from render and state.
-      const getResult = () => {
-        if (mapStateToProps instanceof Function) {
-          props = {...props, ...mapStateToProps(state)};
-        }
-        if (mapDispatchToProps instanceof Function) {
-          props = {...props, ...mapDispatchToProps(dispatch)};
-        }
-        return render({props, state, dispatch});
-      };
-
-      // calls if not cached or has some changes in state. (memoize)
-      if (isChanged) {
-        cache[render] = {
-          result: getResult(),
-          lastProps: props,
-          lastState: state
-        }
+    // getResult from render and state.
+    const getResult = () => {
+      if (mapStateToProps instanceof Function) {
+        props = {...props, ...mapStateToProps(state)};
       }
-
-      return cache[render].result;
+      if (mapDispatchToProps instanceof Function) {
+        props = {...props, ...mapDispatchToProps(dispatch)};
+      }
+      return render({props, state, dispatch});
     };
+
+    // calls if not cached or has some changes in state. (memoize)
+    if (isChanged) {
+      cache[render] = {
+        result: getResult(),
+        lastProps: props,
+        lastState: state
+      }
+    }
+
+    return cache[render].result;
   };
 
   // react-redux like syntax.
